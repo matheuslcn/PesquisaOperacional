8c32d2a1cd961bb53fe576390eb754dc4c5b697a
matheuslcn <matheuslcn@id.uff.br>
2025-01-15 19:30:00 -0300

    algoritmo terminado, falta calibrar os parametros do SA

--- PAS/simulated-annealing.py
+++ PAS/simulated-annealing.py
@@ -28,7 +28,7 @@ class PAS(object):
             lines = f.readlines()
             n_timeslots, n_classes, n_classrooms = map(int, lines[0].split())
             for i in range(1, n_timeslots+1):
-                self.timeslots.append(lines[i])
+                self.timeslots.append(lines[i].strip())
             for i in range(n_timeslots+1, n_timeslots+1+n_classes):
                 class_ = lines[i].split()
                 self.classes.append((class_[0], class_[1], class_[2], int(class_[3])))
@@ -51,7 +51,7 @@ class PAS(object):
     @staticmethod
     def maximize(pPAS: 'PAS', sol: SolutionPAS) -> float:
         eval = 0
-        penalty = 10  # Penalização fixa para violações
+        penalty = 20  # Penalização fixa para violações
 
         # Avaliação de alocações válidas
         for t in range(len(sol.timeslots)):
@@ -59,37 +59,40 @@ class PAS(object):
                 for r in range(len(sol.classrooms)):
                     if sol.solution[t][r][c] == 1:                    
                         # Penalizações
+                        time, room, capacity = 0, 0, 0
                         if sol.timeslots[t] != sol.classes[c][1]:  # Horário incompatível
-                            eval -= penalty
+                            time = 1
                         if sol.classes[c][2] != sol.classrooms[r][1]:  # Tipo incompatível
-                            eval -= penalty
+                            room = 1
                         if sol.classes[c][3] > sol.classrooms[r][2]:  # Capacidade insuficiente
-                            eval -= penalty
+                            capacity = 1
+                        eval -= (5*time + 5*room + 5*capacity)*(time + room + capacity)
                         
                         # Recompensas
-                        eval += 5  # Alocação básica válida
+                        time, room, capacity = 0, 0, 0
                         if sol.timeslots[t] == sol.classes[c][1]:
-                            eval += 5  # Horário ideal
+                            time = 1
                         if sol.classes[c][2] == sol.classrooms[r][1]:
-                            eval += 5  # Tipo ideal
+                            room = 1
                         if sol.classes[c][3] <= sol.classrooms[r][2]:
-                            eval += 5  # Capacidade suficiente
+                            capacity = 1
+                        eval += 5*(time + room + capacity)
 
         # Restrições de unicidade
-        for c in range(len(sol.classes)):
-            for t in range(len(sol.timeslots)):
-                if sum(sol.solution[t][r][c] for r in range(len(sol.classrooms))) > 1:
-                    eval -= penalty  # Turma alocada a várias salas no mesmo horário
-
-        for c in range(len(sol.classes)):
-            for r in range(len(sol.classrooms)):
-                if sum(sol.solution[t][r][c] for t in range(len(sol.timeslots))) > 1:
-                    eval -= penalty  # Turma alocada a vários horários na mesma sala
-
+        # Somente uma classe por sala e por horário
         for t in range(len(sol.timeslots)):
-            for r in range(len(sol.classrooms)):
-                if sum(sol.solution[t][r][c] for c in range(len(sol.classes))) > 1:
-                    eval -= penalty  # Sala ocupada por mais de uma turma no mesmo horário
+            for c in range(len(sol.classrooms)):
+                if sum(sol.solution[t][c]) > 1:
+                    eval -= penalty
+        # Somente uma classe
+        for c in range(len(sol.classes)):
+            cs = []
+            for t in range(len(sol.timeslots)):
+                for r in range(len(sol.classrooms)):
+                    cs.append(sol.solution[t][r][c])
+            result = sum(cs)
+            if result > 1:
+                eval -= result*penalty
         return eval
 
 class MoveBitFlip(Move):
@@ -117,12 +120,31 @@ class NSBitFlip(object):
 ## ================================================
 ## ================================================
 
-pPAS = PAS()
-pPAS.load('teste.txt')
-pPAS.engine.setup(pPAS)
-pPAS.engine.add_ns_class(pPAS, NSBitFlip) 
-list_idx = pPAS.engine.create_component_list("[ OptFrame:NS 0 ]", "OptFrame:NS[]")
-sa = BasicSimulatedAnnealing(pPAS.engine, 0, 0, list_idx, 0.98, 100, 99999)
-sout = sa.search(10.0)
-print("Best solution: ",   sout.best_s)
-print("Best evaluation: ", sout.best_e)
+if __name__ == "__main__":
+    pPAS = PAS()
+    pPAS.load('teste.txt')
+    pPAS.engine.setup(pPAS)
+    pPAS.engine.add_ns_class(pPAS, NSBitFlip) 
+    list_idx = pPAS.engine.create_component_list("[ OptFrame:NS 0 ]", "OptFrame:NS[]")
+    sa = BasicSimulatedAnnealing(pPAS.engine, 0, 0, list_idx, 0.98, 100, 99999)
+    sout = sa.search(15.0)
+    print("Best solution: ",   sout.best_s)
+    print("Best evaluation: ", sout.best_e)
+    sol = sout.best_s
+    not_allocated = []
+    for c in range(len(sol.classes)):
+            cs = []
+            for t in range(len(sol.timeslots)):
+                for r in range(len(sol.classrooms)):
+                    cs.append(sol.solution[t][r][c])
+            if sum(cs) < 1:
+                not_allocated.append(sol.classes[c])
+    print("Not allocated classes: ", not_allocated)
+                
+
+
+# [[[1,0,0],[0,1,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,1],[[0,0,0],[0,0,0],[0,0,0]]]
+# [[[1,0,0],[0,0,0],[0,1,0]],[[0,0,0],[0,0,0],[0,0,1],[[0,0,0],[0,0,0],[0,0,0]]]
+
+#[[[[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]], [[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]], [[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]]], [[[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]], [[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]], [[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]]], [[[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]], [[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]], [[('Turma1', '07:00', 'T', 35), ('Turma2', '07:00', 'P', 30), ('Turma3', '09:00', 'T', 45)]]]]
+# [[[0, 0, 1], [1, 1, 0], [1, 0, 1]], [[1, 1, 0], [0, 0, 0], [1, 0, 0]], [[1, 1, 0], [1, 0, 1], [1, 0, 1]]]
